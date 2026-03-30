738409c09d533e81f867c2a871440a2b59d48d47
Juan <juancruzdillon123@gmail.com>
2026-03-30 01:14:13 -0300

    vercel analytics

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import { createPinia } from 'pinia'
 import { router } from './router/index.js'
 import App from './App.vue'
 import './style.css'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 const app = createApp(App)
 app.use(createPinia())
